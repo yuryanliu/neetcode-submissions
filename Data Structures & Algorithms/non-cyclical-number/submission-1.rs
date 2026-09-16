d2f641e40420e0fc843f48b04296da591dbdcd4b
yuryanliu <46794144+yuryanliu@users.noreply.github.com>
2026-09-15 17:28:21 -0700

    Add: non-cyclical-number - submission-1

---
+++ Data Structures & Algorithms/non-cyclical-number/submission-1.rs
@@ -0,0 +1,24 @@
+impl Solution {
+    pub fn is_happy(n: i32) -> bool {
+        let mut exists = HashSet::new();
+        let mut n = n;
+        while n != 0 && n != 1 {
+            n = Solution::compute(n);
+            if exists.contains(&n) {
+                return false;
+            }
+            exists.insert(n);
+        }
+        n == 1
+    }
+
+    fn compute(mut n: i32) -> i32 {
+        let mut sum = 0;
+        while n != 0 {
+            let d = n%10;
+            n /= 10;
+            sum += d*d;
+        }
+        sum
+    }
+}
