f3c674d590b7cbcfc0fec46b4d7ee00e96f5b354
yuryanliu <46794144+yuryanliu@users.noreply.github.com>
2026-09-20 09:28:54 -0700

    Add: jump-game - submission-3

---
+++ Data Structures & Algorithms/jump-game/submission-3.rs
@@ -0,0 +1,17 @@
+impl Solution {
+    pub fn can_jump(nums: Vec<i32>) -> bool {
+        assert!(!nums.is_empty());
+        let n = nums.len();
+        let mut i = 0;
+        let mut j = i + nums[i] as usize;
+        while i != j && j < n - 1 {
+            let mut l = j;
+            for k in i+1..=j {
+                l = l.max(k + nums[k] as usize).min(n - 1);
+            }
+            i = j;
+            j = l;
+        }
+        j >= n - 1
+    }
+}
